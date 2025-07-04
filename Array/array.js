5b3c0dcfe8893532e016f655e36ead702a6433f3
vinayg2509 <vinayg2509@gmail.com>
2025-07-04 14:31:40 +0530

    [Vinay]Added method to remove element from first

--- Array/array.js
+++ Array/array.js
@@ -1,62 +1,76 @@
-console.log("Linear data structure")
+console.log("Linear data structure");
 
 //!-----------------------Array--------------------------
 
-class MyArray
+class MyArray 
 {
-    constructor(array)
+    constructor(array = []) 
     {
-        this.array=array
+        this.array = array;
     }
 
     //* to find the length of the array
-    length()
+    length() {
+        let count = 0;
+        while (this.array[count] != undefined) 
         {
-            let count=0
-            while(array[count]!=undefined)
-            {
-                count++
-            }
-            return count;
+            count++;
         }
+        return count;
+    }
 
-    //*To traverse an array
+    //* To traverse an array
     displayElement()
     {
-       
-        if(this.length()==0)
-        {
+        let length = this.length();
+        if (length == 0) 
+            {
             console.log(`Array is empty`);
-        }
-        else
-        {
-             for(let index=0;index<this.length();index++)
-            {
-                console.log(array[index]);
             }
-        } 
-       
+            else 
+            {
+            for (let index = 0; index < length; index++) 
+                {
+                    console.log(this.array[index]);
+                }
+            }
     }
-    //*To add first (unshift)
-    customUnshift(array,value)
+
+    //* To add at first (unshift)
+    customUnshift(value) 
     {
-        for(let index=this.length();index>=0;index--)
-            {
-                array[index]=array[index-1]
-            }
-            array[0]=value
+        let length = this.length();
+        for (let index = length; index > 0; index--) 
+        {
+            this.array[index] = this.array[index - 1];
+        }
+        this.array[0] = value;
+    }
+
+    //* To remove from first (shift)
+    customShift() 
+    {
+        let first = this.array[0];
+        let length = this.length();
+        for (let index = 0; index < length - 1; index++) 
+        {
+            this.array[index] = this.array[index + 1];
+        }
+        this.array.length = length - 1;
+        return first;
     }
 }
 
-let array= new MyArray()
+// Testing
+let array = new MyArray();
+
+array.customUnshift(20);
+array.customUnshift(10);
+array.customUnshift(30);
+array.customUnshift(56);
+
+console.log(`Length of an array is: ${array.length()}`);
+console.log(`Removed element from an array is : ${array.customShift()}`);
 
 
-array.customUnshift(array,20)
-array.customUnshift(array,10)
-array.customUnshift(array,30)
-
-console.log(`Length of an array is:${array.length()}`);
-array.displayElement()
-
-
-
+array.displayElement();
