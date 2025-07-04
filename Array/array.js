16d59f9b226c5b66af28b3bd3e31c70fee76ff1a
vinayg2509 <vinayg2509@gmail.com>
2025-07-04 12:07:59 +0530

    [Vinay]Added:Method to find the length of an array

--- Array/array.js
+++ Array/array.js
@@ -1,1 +1,28 @@
 console.log("Linear data structure")
+
+//!-----------------------Array--------------------------
+
+class Array
+{
+    constructor(array)
+    {
+        this.array=array
+    }
+
+    //* to find the length of the array
+    length()
+        {
+            let count=0
+            while(array[count]!=undefined)
+            {
+                count++
+            }
+            return count;
+        }
+
+}
+
+let array= new Array()
+console.log(`Length of an array is:${array.length()}`);
+
+
